8f286c6ae3455c2fd7f5b44db9114ce7eaafab48
yasuabe <yasu@yasuabe.net>
2017-03-19 07:42:25 +0900

    solve pe002

--- src/pe/Pe002.scala
+++ src/pe/Pe002.scala
@@ -1,8 +1,11 @@
 package pe
 
-/**
-  * Created by xad on 2017/03/19.
-  */
-object Pe002 {
+import util.Utils._
 
+object Pe002 {
+  def solve(a: Int, b: Int, sum: Long): Long =
+    if (b >= 4000000) sum
+    else solve(b, a + b, sum + (if (b % 2 == 0) b else 0))
+
+  def main(args: Array[String]) = run(solve(1, 2, 0))
 }
