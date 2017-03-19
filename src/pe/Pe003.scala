542abf823affe012e253d2ebc16e021ce5c4c9de
yasuabe <yasu@yasuabe.net>
2017-03-19 13:38:12 +0900

    pe003

--- src/pe/Pe003.scala
+++ src/pe/Pe003.scala
@@ -1,8 +1,14 @@
 package pe
 
-/**
-  * Created by xad on 2017/03/19.
-  */
-object Pe003 {
+import util.Primes._
 
+object Pe003 {
+  def divide(n: Long, ps: Stream[Long]): Long = {
+    val p #:: pt = ps
+    if (n == p) p
+    else if (n < p * p) n
+    else if (n % p == 0) divide(n / p, ps)
+    else divide(n, pt)
+  }
+  def main(args: Array[String]) = run(divide(600851475143L, primes()))
 }
--- src/pe/package.scala
+++ src/pe/package.scala
@@ -2,4 +2,5 @@ import util.Utils
 
 package object pe {
   def run[T](f: => T) = Utils.run(f)
+  def divMod(n: Long, m: Long): (Long, Long) = (n / m, n % m)
 }
