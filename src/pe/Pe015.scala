dc36c55c048863b7ec2d5f01311b275e96ec8ebe
yasuabe <yasu@yasuabe.net>
2017-03-22 02:36:28 +0900

    solve 015

---
+++ src/pe/Pe015.scala
@@ -0,0 +1,17 @@
+package pe
+
+object Pe015 {
+  type Fraction = (Long, Long)
+  def frac(n: Long, d: Long): Fraction = {
+    val g = gcd(n, d)
+    (n / g, d / g)
+  }
+  def multiply(f1: Fraction, f2: Fraction): Fraction =
+    frac(f1._1 * f2._1, f1._2 * f2._2)
+
+  def solve = {
+    val fh :: ft = (1L to 20L).map(d => frac(d + 20, d)).toList
+    ft.foldLeft(fh)((acc, e) => multiply(acc, e))
+  }
+  def main(args: Array[String]): Unit = run(solve._1)
+}
--- src/pe/package.scala
+++ src/pe/package.scala
@@ -1,6 +1,7 @@
 import util.Utils
 
 package object pe {
+  def gcd(a: Long, b: Long): Long = if (b == 0) a else gcd(b, a % b)
   def run[T](f: => T) = Utils.run(f)
   def divMod(n: Long, m: Long): (Long, Long) = (n / m, n % m)
   def loadResource(name: String): String =
