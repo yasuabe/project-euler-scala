8404820591f357e90897fa9046af611c0cf7faa9
yasuabe <yasu@yasuabe.net>
2017-03-25 01:25:01 +0900

    solve 022

---
+++ src/pe/Pe022.scala
@@ -0,0 +1,24 @@
+package pe
+
+import util.PairingHeap
+
+object Pe022 {
+  def value(name: String): Long = name.toCharArray.map(_ - 'A' + 1).sum
+
+  def solve(names: List[String]): Long = {
+    import PairingHeap._
+
+    def go(pos: Int, ph: PairingHeap[String], result: Long): Long = ph match {
+      case util.Empty => result
+      case util.Heap(elem, subHeaps) =>
+        go(pos + 1, mergePairs(subHeaps), pos * value(elem) + result)
+    }
+    val namesHeap = names.foldRight[PairingHeap[String]](util.Empty)(insert)
+
+    go(1, namesHeap, 0)
+  }
+  def namesIn(text: String): List[String] =
+    text.split(",").map(s => s.substring(1, s.length - 1)).toList
+
+  def main(args: Array[String]): Unit = run(solve(namesIn(loadResource("p022_names.txt"))))
+}
