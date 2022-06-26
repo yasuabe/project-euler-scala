7e8eeca550d12b8d640399060d51dd491532faa3
yasuabe2613 <yasu@yasuabe.net>
2022-06-27 00:08:20 +0900

    touch up Pe001

--- src/main/scala/pe/Pe001.scala
+++ src/main/scala/pe/Pe001.scala
@@ -10,7 +10,7 @@ object Pe001:
     val l = (limit - 1) / n
     (1 + l) * l / 2 * n
   )
-  def calc: Reader[Long, Long] = (3, 5, 15).map(sumOfMultiples).mapN(_ + _ - _)
-  def solve = calc.run(1000)
+  // def calc: Reader[Long, Long] = (3, 5, 15).map(sumOfMultiples).mapN(_ + _ - _)
+  def solve(limit: Long) = (3, 5, 15).map(sumOfMultiples).mapN(_ + _ - _).run(limit)
 
-  @main def main01 = run(solve)
+  @main def main01 = run(solve(1000))
--- src/test/scala/pe/Pe001Suite.scala
+++ src/test/scala/pe/Pe001Suite.scala
@@ -3,4 +3,5 @@ package pe
 import munit.FunSuite
 
 class Pe001Suite extends FunSuite:
-  test("solve 001 correctly") { assertEquals(Pe001.solve, 233168L) }
+  test("solve 001 sample"   ) { assertEquals(Pe001.solve(10)  , 23L    ) }
+  test("solve 001 correctly") { assertEquals(Pe001.solve(1000), 233168L) }
