0aa731ca0490b282e26946f07c6ee01773b6fb2f
yasuabe2613 <yasu@yasuabe.net>
2022-06-30 03:22:11 +0900

    touch up LazyPrimes

--- src/main/scala/util/LazyPrimes.scala
+++ src/main/scala/util/LazyPrimes.scala
@@ -1,33 +1,35 @@
 package util
 
+import mouse.boolean.*
+
 object LazyPrimes:
   // TODO: refactoring
   import PQueue._
 
-  sealed trait PQueue:
-    def merge(another: PQueue): PQueue
-
-  case object PNil extends PQueue:
-    def merge(another: PQueue): PQueue = another
-
-  case class PCons(ns: LazyList[Long], qs: List[PQueue]) extends PQueue:
-    def join(q2: PQueue): PQueue = PCons(ns, q2 :: qs)
-    def merge(y: PQueue): PQueue = y match {
-      case PNil      => this
-      case y1: PCons => if (ns.head <= y1.ns.head) join(y1) else y1.join(this)
-    }
+  enum PQueue:
+    case PCons(ns: LazyList[Long], qs: List[PQueue])
+    case PNil
 
   object PQueue:
+    extension (q: PCons)
+      infix def join(q2: PQueue): PQueue = PCons(q.ns, q2 :: q.qs)
+
+    extension (q: PQueue)
+      def merge(another: PQueue): PQueue = q match
+        case PNil             => another
+        case q@ PCons(ns, qs) => another match
+          case PNil     => q
+          case y: PCons => if ns.head <= y.ns.head then q join y else y join q
+
     def enqueue(ns: LazyList[Long], q: PQueue): PQueue = PCons(ns, Nil).merge(q)
-    def mergeAll(qs: List[PQueue]): PQueue = qs match {
+    def mergeAll(qs: List[PQueue]): PQueue = qs match
       case Nil            => PNil
       case q :: Nil       => q
       case q1 :: q2 :: qs => q1.merge(q2).merge(mergeAll(qs))
-    }
 
   type Wheel = LazyList[Long]
 
-  def cycle(ns: LazyList[Long]): LazyList[Long] = ns #::: cycle(ns)
+  def cycle(ns: Wheel): Wheel                = ns #::: cycle(ns)
   def spin(start: Long, wheel: Wheel): Wheel = cycle(wheel).scan(start)(_ + _)
 
   def comps(ns: LazyList[Long]): LazyList[Long] = comps(ns.head, ns)
@@ -37,21 +39,23 @@ object LazyPrimes:
   def sieve(nns: LazyList[Long], q: PQueue): LazyList[Long] = (nns, q) match
     case (n #:: ns, PNil)                => n #:: sieve(ns, enqueue(comps(nns), PNil))
     case (n #:: ns, PCons(m #:: ms, qs)) =>
-      if (n < m) n #:: sieve(ns, enqueue(comps(nns), q))
-      else       sieve(if (m == n) ns else nns, enqueue(ms, mergeAll(qs)))
+      if n < m
+      then n #:: sieve(ns, enqueue(comps(nns), q))
+      else sieve((m == n).fold(ns, nns), enqueue(ms, mergeAll(qs)))
     case (_, _)                          => ???
 
-  def next(ps: LazyList[Long], xs: LazyList[Long]): (LazyList[Long], LazyList[Long]) = ps match
-    case p #:: _ =>
-      val (y #:: ys) = cycle(xs)
-      val py = p + y
-      (py #:: ps, cancel(ps.product, p, py, ys))
+  def next(ps: LazyList[Long], xs: LazyList[Long]): (LazyList[Long], LazyList[Long]) =
+    val p #:: _  = ps
+    val y #:: ys = cycle(xs)
+    val py       = p + y
+    (py #:: ps, cancel(ps.product, p, py, ys))
 
   def cancel(m: Long, p: Long, n: Long, xs: LazyList[Long]): LazyList[Long] = (m, xs) match
     case (0, _)        => LazyList.empty[Long]
     case (_, x #:: ys) =>
       val nx = n + x
-      if ((nx % p) > 0) x #:: cancel(m - x, p, nx, ys)
+      if (nx % p) > 0
+      then x #:: cancel(m - x, p, nx, ys)
       else cancel(m, p, n, (x + ys.head) #:: ys.tail)
 
   def wheel(n: Int): (LazyList[Long], LazyList[Long]) =
@@ -64,12 +68,14 @@ object LazyPrimes:
 
   def primeFactors(n: Long, primes: => LazyList[Long] = LazyPrimes.primes()): List[(Long, Int)] =
     def go(n: Long, ps: LazyList[Long], result: List[(Long, Int)]): List[(Long, Int)] =
-      if (n == 1) result
-      else ps match { case ph #:: pt =>
-        if (n % ph == 0) result match {
-          case (p, c)  :: rt if p == ph => go(n / ph, ps, (ph, c + 1) :: rt)
-          case _ => go(n / ph, ps, (ph, 1) :: result)
-        } else if (n < ph * ph) (n, 1) :: result
-        else go(n, pt, result)
-      }
+      if n == 1 then result
+      else
+        val ph #:: pt = ps
+        if n % ph == 0 then
+          result match
+            case (p, c)  :: rt if p == ph => go(n / ph, ps, (ph, c + 1) :: rt)
+            case _                        => go(n / ph, ps, (ph, 1) :: result)
+        else if (n < ph * ph) then (n, 1) :: result
+        else                       go(n, pt, result)
+
     go(n, primes, Nil)
