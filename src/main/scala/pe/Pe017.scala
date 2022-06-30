465a4b6043bda9ded870c75066862af7ff255c08
yasuabe2613 <yasu@yasuabe.net>
2022-07-01 02:10:23 +0900

    update 017

--- src/main/scala/pe/Pe017.scala
+++ src/main/scala/pe/Pe017.scala
@@ -1,6 +1,6 @@
 package pe
 
-object Pe017 {
+object Pe017:
   def lengths(s: String*) = s.map(_.length)
   val digit1 = lengths(
     "one",
@@ -26,33 +26,25 @@ object Pe017 {
   val digit2 = lengths(
     "twenty",
     "thirty",
-    "fourty",
+    "forty",
     "fifty",
     "sixty",
     "seventy",
     "eighty",
     "ninety")
-  val digit3 = lengths(
-    "hundred",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen")
+
   val len100  = "hundred".length
   val lenAND  = "and".length
   val len1000 = ("one" + "thousand").length
 
-  def solve = {
+  def solve =
     val ls1_9   = digit1.sum
     val ls10_19 = digits10to19.sum
     val ls20_99 = digit2.map(_ * 10 + ls1_9).sum
     val ls1_99  = ls1_9 + ls10_19 + ls20_99
-    ls1_99 * 10 + lenAND * 99 * 9 + (ls1_9 + len100 * 9) * 100 + len1000
-  }
-  def main(args: Array[String]): Unit = run(solve)
-}
+    ls1_99 * 10                + // 下二桁
+    lenAND * 99 * 9            + // 100以上、100で割り切れない数に AND がつく
+    (ls1_9 + len100 * 9) * 100 + //
+    len1000                      // "one thousand"
+
+  @main def main017: Unit = run(solve)
---
+++ src/test/scala/pe/Pe017Suite.scala
@@ -0,0 +1,6 @@
+package pe
+
+import munit.FunSuite
+
+class Pe017Suite extends FunSuite:
+  test("solve 017 correctly") { assertEquals(Pe017.solve, 21124) }
